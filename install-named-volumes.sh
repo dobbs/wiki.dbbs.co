31d019b767c3872fac0a503d95920d1dd1ac0a33
Eric Dobbs <dobbs@aiki.local>
2017-11-25 22:43:45 -0700

    install-named-volumes.sh now creates proxy.d & proxy.certs
    small steps to upgrade proxy to use dobbs/proxy:0.10.10
    
    * tagged the current production version manually
    
    * create and chown /etc/proxy.d & /etc/proxy.certs
    
    * updated docker-compose.yml with tag

--- install-named-volumes.sh
+++ install-named-volumes.sh
@@ -25,23 +25,36 @@ create-docker-environment() {
 
 create-named-volumes() {
     docker volume create --name proxy.$DROPLET
+    docker volume create --name proxy.d.$DROPLET
+    docker volume create --name proxy.certs.$DROPLET
     docker volume create --name wiki.$DROPLET
 }
 
 open-portal-to-named-volumes() {
-    docker run --name ping -d \
+    docker run \
+           --name portal \
+           -d \
+           --init=true \
            -v proxy.$DROPLET:/proxy \
-           buildpack-deps:jessie-curl ping -i 60 localhost
+           -v proxy.d.$DROPLET:/proxy.d \
+           -v proxy.certs.$DROPLET:/proxy.certs \
+           --user=root \
+           --entrypoint=/usr/bin/tail \
+           dobbs/proxy:0.10.10 \
+           -f /dev/null
 }
 
 install-configs-in-named-volumes() {
     cd $COMPOSE_DIR
-    docker cp proxy/Caddyfile ping:/proxy/Caddyfile
-    docker-compose run --rm --user root web chown -R app:app .wiki
+    docker cp Caddyfile portal:/proxy/Caddyfile
+    docker exec portal \
+           sh -c 'chown -R caddy:nogroup /proxy /proxy.d /proxy.certs'
+    docker-compose run --rm --user root farm chown -R app:app .wiki
 }
 
 close-portal-to-named-volumes() {
-    docker rm -f ping
+    docker stop portal
+    docker rm portal
 }
 
 create-environment $@
